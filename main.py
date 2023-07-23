dcfeaf4a814b6e8730799a5f1a09ee293d0beda8
obijzbo <nahiyanmubashshir@gmail.com>
2023-07-23 06:51:09 +0600

    yml file update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
+ip = '192.168.56.30'
 # Render the homepage with buttons
 @app.route('/')
 def index():
@@ -10,17 +11,17 @@ def index():
 # Redirect to post_app
 @app.route('/redirect-7000')
 def redirect_7000():
-    return redirect('http://post-app:7000')
+    return redirect(f'http://{ip}:30110')
 
 # Redirect to music_app
 @app.route('/redirect-8000')
 def redirect_8000():
-    return redirect('http://music-app:8000')
+    return redirect(f'http://{ip}:30120')
 
 # Redirect to documents_app
 @app.route('/redirect-9000')
 def redirect_9000():
-    return redirect('http://documents-app:9000')
+    return redirect(f'http://{ip}:30130')
 
 if __name__ == '__main__':
     # If running with Gunicorn, you can start the app using Gunicorn like this:
